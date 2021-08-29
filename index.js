f42c5021148b1099c054333e5a2924a0218a837d
Monishkumar02 <monishkumarp147@gmail.com>
2021-08-29 17:46:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let a=10;
+let b=20;
+let sum=a+b;
+console.log(sum);
+for (let i=0; i<=10; i++) console.log(i);
+
+document.getElementsByTagName("h1")[0].addEventListener("mouseleave", () => {
+  alert("You just moved the cursor from Web Development Workshop lol!!");
+});
+
+
+function any() {
+  alert("I'm an alert box !!")
+}
